e950c11a35e830590b5c545b85d84f4de962ac06
anailson <arsantos315@gmail.com>
2022-12-27 14:38:56 -0300

    Consultas com filtros

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -24,7 +24,13 @@ public class CadastroDeProduto {
 			Produto p = produtoDao.buscarPorId(1l);
 			System.out.println(p.getPreco());
 			
-			List<Produto> todos = produtoDao.buscarTodos();
+			//List<Produto> todos = produtoDao.buscarTodos();
+			//todos.forEach(p2 -> System.out.print(p.getNome()));
+			
+			//List<Produto> todos = produtoDao.buscarPorNome("Xiaomi Redmi");
+			//todos.forEach(p2 -> System.out.print(p.getNome()));
+			
+			List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
 			todos.forEach(p2 -> System.out.print(p.getNome()));
 			
 		}
