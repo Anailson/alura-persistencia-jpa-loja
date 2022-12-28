d874192fee1525fc49af61bb94cd8c3b9a8e9b00
anailson <arsantos315@gmail.com>
2022-12-28 13:30:56 -0300

    Mapeando novas entidades

---
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,75 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal valorTotal;
+	private LocalDate data = LocalDate.now();// Pegando data atual
+
+	@ManyToOne
+	private Cliente cliente;
+
+	public Pedido() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pedido(Cliente cliente) {
+		super();
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	/*
+	 * @Column(name = "desc") //Ser fosse deixar o nome dessa forma para salvar no
+	 * banco de dados
+	 * 
+	 */
+
+}
